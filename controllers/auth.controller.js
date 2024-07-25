cf2d29dba30e0b3062b881e2d3fb0bb6e0d1f75f
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-25 12:50:28 +0100

    Commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,6 +5,7 @@ const User = require("../models/user.model");
 const Wallet = require("../models/wallet.model");
 const validateRegisterUser = require("../validations/user.validation");
 const { hashPassword } = require("../utils/helper.util");
+const { sendEmail } = require("../services/email");
 const { v4: uuidv4 } = require("uuid");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -61,91 +62,211 @@ exports.register = async (req, res) => {
       user_id: userID,
     });
 
-    const msg = {
-      to: email,
-      from: process.env.EMAIL_FROM,
-      subject: "Welcome to Our Service",
-      templateId: "d-1234567890abcdef1234567890abcdef", // Replace with your SendGrid template ID
-      dynamic_template_data: {
-        name: user.name,
+    //give them 1000 on signup
+    credit(200, userID, `Wallet funding for signup credits`);
+
+    //send as sms
+    //send as email
+    sendEmail(
+      email,
+      "OTP",
+      `Hi ${surname}, Welcome, your to Geocodec Shipping Services`
+    );
+
+    res.status(201).json({
+      status: true,
+      message: registerMessage,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+exports.verifyUserAccount = async (req, res) => {
+  const { otp, email } = req.params;
+  if (!otp || !email) {
+    res.status(400).json({
+      status: false,
+      message: "Bad request",
+    });
+    return;
+  }
+  try {
+    const otpData = await OtpModel.findOne({
+      where: {
+        email_or_phone: email,
+        otp: otp,
+        otp_type: OtpEnum.REGISTRATION,
       },
+    });
+    if (!otpData) {
+      res.status(400).json({
+        status: false,
+        message: invalidOtp,
+      });
+      return;
+    }
+    //check if otp has expired
+
+    await User.update(
+      {
+        isOtpVerified: true,
+      },
+      {
+        where: {
+          email: email,
+        },
+      }
+    );
+
+    await OtpModel.destroy({
+      where: {
+        email_or_phone: email,
+        otp_type: OtpEnum.REGISTRATION,
+      },
+    });
+
+    res.status(200).json({
+      status: true,
+      message: "Account verified successfully",
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+exports.getUserDetails = async (req, res) => {
+  const { user_id } = req.params;
+  if (!user_id) {
+    res.status(400).json({
+      status: false,
+      message: "Bad request",
+    });
+    return;
+  }
+  try {
+    const user = await User.findOne({
+      attributes: [
+        "name",
+        "email",
+        "phone",
+        "address",
+        "city",
+        "zip_code",
+        "country",
+      ],
+      where: {
+        user_id: user_id,
+      },
+    });
+    if (!user) {
+      res.status(400).json({
+        status: false,
+        message: "User not found",
+      });
+      return;
+    }
+    res.status(200).json({
+      status: true,
+      data: user,
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+exports.updateUserProfile = async (req, res) => {
+  const { user_id } = req.params;
+  if (!user_id) {
+    return res.status(400).json({
+      status: false,
+      message: "bad request",
+    });
+  }
+  ///use joi to validate the request body
+  const { error } = validateRegisterUser(req.body);
+  if (error !== undefined) {
+    res.status(400).json({
+      status: false,
+      message: error.details[0].message || "Bad request",
+    });
+    return;
+  }
+  try {
+    await User.update(req.body, {
+      where: {
+        user_id: user_id,
+      },
+    });
+
+    res.status(200).json({
+      status: true,
+      message: "user profile updated successfully",
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+exports.userLogin = async (req, res) => {
+  //login user
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) throw new Error("All fields are required");
+    const checkIfUserExists = await User.findOne({
+      where: { email: email },
+    });
+    if (checkIfUserExists == null) throw new Error("Invalid email or password");
+    let payload;
+    let accessToken;
+
+    const dataToaddInMyPayload = {
+      email: checkIfUserExists.email,
+      _id: uuidv4(),
     };
 
-    await sgMail.send(msg);
-
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-exports.login = (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.status(400).json({ message: info.message });
-
-    req.login(user, { session: false }, (err) => {
-      if (err) return next(err);
-      const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
-        expiresIn: "1h",
+    const compareHash = await bcrypt.compare(
+      password,
+      checkIfUserExists.password_hash
+    );
+    if (!compareHash) throw new Error("Invalid email or password");
+    if (!checkIfUserExists.isOtpVerified) {
+      res.status(400).json({
+        status: false,
+        level: 2,
+        message: "Account not verified",
       });
-      return res.json({ user, token });
-    });
-  })(req, res, next);
-};
-
-exports.updateUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, email, password, role, isActive } = req.body;
-
-    const user = await User.findByPk(id);
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    if (password) {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
+      return;
     }
+    const token = await jwt.sign(dataToaddInMyPayload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
-    await user.update({ name, email, role, isActive });
-
-    res.status(200).json(user);
+    res.status(200).json({
+      status: true,
+      message: loginMessage,
+      token: token,
+    });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({
+      status: false,
+      message: error.message || "Internal server error",
+    });
   }
 };
 
-exports.deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    await user.destroy();
-
-    res.status(200).json({ message: "User deleted" });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-exports.getUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
