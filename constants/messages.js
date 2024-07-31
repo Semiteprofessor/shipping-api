97039b1e2bd08323996f79b609d0280af9a243a1
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-31 23:06:29 +0100

    Commit

--- constants/messages.js
+++
@@ -1,8 +0,0 @@
-const messages = {
-    loginMessage: "User logged-in successfully",
-    registerMessage: "Guy, Your account has been created successfully, an otp has been sent to your email",
-    userExists: "User with the Phone/Email exists",
-    notFoundMessage: "What you seek is beyond this globe",
-}
-
-module.exports = messages;
